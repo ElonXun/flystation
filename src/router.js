b8486e58a3458711a960f0454ee8d221be293811
fmn <cdnxyf@gmail.com>
2017-07-26 17:39:32 +0800

    Revert "Revert "first commit""
    This reverts commit 074db60e9c234f9060f9f1431480534b06b61f5f.

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router, Route } from 'dva/router';
+import Homepage from './routes/Homepage';
+import Products from './routes/Products';
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Route path="/" component={Homepage} />
+      <Route path="/products" component={Products} />
+    </Router>
+  );
+}
+
+export default RouterConfig;
