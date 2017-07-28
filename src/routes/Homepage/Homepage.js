1db7db8a3f552194ceca16fa9ab361ec9b773914
fmn <cdnxyf@gmail.com>
2017-07-28 17:29:01 +0800

    mock数据以及改写部分公共组件

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './Homepage.css';
-import { Menu, Icon, Row, Col } from 'antd';
+import { Tag, Row, Col } from 'antd';
 import Navigator from '../../components/public/navigator/navigator.js';
+import RightNavHeader from '../../components/public/rightNavHeader/navHeader.js';
+import ArticleSelect from '../../components/public/articleSelect/articleSelect';
+import data from '../../data.js';
+
+const article = {
+  blogTitle: '腾讯传评',
+  createTime: '2017-01-23 14:35:31',
+  blogReadNum: '234',
+  blogThumbnail: 'http://flystation.image.alimmdn.com/blog/blogBackground/tencentBlog.jpg',
+};
 
 const Homepage = () => {
   return (
@@ -12,44 +22,41 @@ const Homepage = () => {
         <Row>
           <Col xs={0} md={3} />
           <Col xs={24} md={18}>
-            <div>
-            <Row gutter={16}>
-              <Col span={16}>
-                <div className={styles.wrap}>
-                   <div className={styles.articleContainer}>
-                     <div className={styles.titleContainer}>
-                       <div className={styles.titleWrap}>
-                         <p className={styles.title}>lantern访问facebook等国外网站</p>
-                         <p className={styles.subTitle}>2017-01-21 22:22:21 阅读量:25</p>
-                       </div>
-                     </div>
-                     <div className={styles.thumbnail}>
-                       <img src='http://flystation.image.alimmdn.com/blog/blogBackground/lantern.png' height={125} width={200} />
-                     </div>
-                   </div>
-                </div>
-              </Col>
-              <Col span={8}>
-                <div className={styles.rightNav}>
-                  <div className={styles.readMostContainer}>
-                    <div className={styles.rightNavHeader}>
-                      <div className={styles.rightNavTitle}>
-                        <Icon type="like-o" /> 阅读最多
+            <div className={styles.main}>
+              <Row gutter={16}>
+                <Col span={16}>
+                  <div className={styles.wrap}>
+                    <ArticleSelect article={article} />
+                    {
+                      data.data.map((val, index) => {
+                        return (<ArticleSelect article={val} />);
+                      })
+                    }
+                  </div>
+                </Col>
+                <Col span={8}>
+                  <div className={styles.rightNav}>
+                    <div className={styles.rightNavContainer}>
+                      <RightNavHeader iconType={'like-o'} navTitle={'阅读最多'} />
+                      <div className={styles.mostReadContent}>
+                        lantern访问facebook等国外网站
+                      </div>
+                      <div className={styles.mostReadContent}>
+                        lantern访问facebook等国外网站
+                      </div>
+                      <div className={styles.mostReadContent}>
+                        lantern访问facebook等国外网站
                       </div>
                     </div>
-                    <div className={styles.mostReadContent}>
-                      lantern访问facebook等国外网站
-                    </div>
-                    <div className={styles.mostReadContent}>
-                      lantern访问facebook等国外网站
-                    </div>
-                    <div className={styles.mostReadContent}>
-                      lantern访问facebook等国外网站
+                    <div className={styles.rightNavContainer}>
+                      <RightNavHeader iconType={'tag-o'} navTitle={'标签'} />
+                      <div className={styles.tagsContainer}>
+                        <Tag color="pink" className={styles.tagWrap}>pink</Tag>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Col>
-            </Row>
+                </Col>
+              </Row>
             </div>
           </Col>
           <Col xs={0} md={3} />
