ef515e181b8d08e79e025aa20048cd86d4e1ab84
fmn <cdnxyf@gmail.com>
2017-08-03 10:41:52 +0800

    删除无用代码

--- src/models/navigator.js
+++ src/models/navigator.js
@@ -8,19 +8,6 @@ export default {
     blogTypeVisibilityFilter: 'SHOW_ALL_BLOG',
   },
   reducers: {
-    /*toRealStuff(state, { payload: type }) {
-      console.log('进入toRealStuff');
-      return state.filter(item => item.blogType === type);
-    },
-    toNote(state, { payload: type }) {
-      console.log('进入toNote');
-      console.log(state);
-      return state.filter(item => item.blogType === type);
-    },
-    toTravel(state, { payload: type }) {
-      console.log('进入toTravel');
-      return state.filter(item => item.blogType === type);
-    },*/
     blogTypeVisibilityFilter(state, { payload: filter }) {
       return Object.assign({}, state, { blogTypeVisibilityFilter: filter });
     },
